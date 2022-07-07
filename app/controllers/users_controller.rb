b8eda177768422356056d978d563e036a3ba7f32
Chris Jones <twoface01@gmail.com>
2022-07-07 16:17:26 +0000

    Search for friends, view their profile and add stocks from their portfolio

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,38 @@
 class UsersController < ApplicationController
   def my_portfolio
     @tracked_stocks = current_user.stocks
+    @user = current_user
   end
   
   def my_friends
     @tracked_friends = current_user.friends
   end
+  
+  def show
+    @user = User.find(params[:id])
+    @tracked_stocks = @user.stocks
+  end
+  
+  def search
+    if params[:friend].present?
+      @friends = User.search(params[:friend])
+      @friends = current_user.except_current_user(@friends)
+      if @friends
+        respond_to do |format|
+          format.js { render partial: 'users/friend_result' }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "User not found."
+          format.js { render partial: 'users/friend_result' }
+        end
+      end
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a name or email address to search."
+        format.js { render partial: 'users/friend_result' }
+      end
+    end
+  end
+  
 end
